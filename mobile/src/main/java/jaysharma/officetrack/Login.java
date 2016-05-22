e4fd36bf68b10b653f2e721a5195143ab689b94c
Jay Sharma <sharmajaym2007@gmail.com>
2016-05-21 19:54:51 -0500

    Implemented Facebook Login for future use Updated the Dashboard to include a spinner listing all existing Wifi configured networks

--- mobile/src/main/java/jaysharma/officetrack/Login.java
+++ mobile/src/main/java/jaysharma/officetrack/Login.java
@@ -1,23 +1,27 @@
 package jaysharma.officetrack;
 
-import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
-import android.net.wifi.WifiConfiguration;
-import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.ArrayAdapter;
-import android.widget.Spinner;
+import android.widget.TextView;
 
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 import com.google.android.gms.appindexing.Action;
 import com.google.android.gms.appindexing.AppIndex;
 import com.google.android.gms.common.api.GoogleApiClient;
 
-import java.util.ArrayList;
-import java.util.List;
 
-public class Dashboard extends AppCompatActivity {
-    public final static String EXTRA_MESSAGE = "jaysharma.office.track.FUCKING_EXTRA_MESSAGE";
+public class Login extends AppCompatActivity {
+
+    private TextView info;
+    private LoginButton loginButton;
+    private CallbackManager callbackManager;
     /**
      * ATTENTION: This was auto-generated to implement the App Indexing API.
      * See https://g.co/AppIndexing/AndroidStudio for more information.
@@ -27,55 +31,45 @@ public class Dashboard extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_dashboard);
-        populateConfiguredWifiSpinner();
+        /*Initialize Facebook SDK*/
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        callbackManager = CallbackManager.Factory.create();
+        setContentView(R.layout.activity_login);
 
+        /*Get TextView and Login Button*/
+        info = (TextView) findViewById(R.id.info);
+        loginButton = (LoginButton) findViewById(R.id.login_button);
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
 
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                info.setText(
+                        "User ID: " + loginResult.getAccessToken().getUserId() + "\n" +
+                                "Auth Token: "
+                                + loginResult.getAccessToken().getToken()
+                );
+            }
+
+            @Override
+            public void onCancel() {
+                info.setText("Login attempt canceled.");
+            }
+
+            @Override
+            public void onError(FacebookException e) {
+                info.setText("Login attempt failed");
+            }
+        });
         // ATTENTION: This was auto-generated to implement the App Indexing API.
         // See https://g.co/AppIndexing/AndroidStudio for more information.
         client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
-    private void populateConfiguredWifiSpinner() {
-
-
-        List list = getSSID();
-
-        // Create a dropdown spinner
-        Spinner wifiListSpinner = (Spinner) findViewById(R.id.configuredWifiSpinner);
-        ArrayAdapter<String> wifiListAdapter = new ArrayAdapter<>(this,
-                android.R.layout.simple_spinner_item, list);
-        wifiListAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        wifiListSpinner.setAdapter(wifiListAdapter);
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
     }
 
-    /**
-     * Get the list of all configured Wifi Network and retrieves SSID of all networks
-     *
-     * @return List of SSID's
-     */
-    private List getSSID() {
-        List<String> list = new ArrayList<>();
-        WifiManager wifiManager = (WifiManager) this.getSystemService(Context.WIFI_SERVICE);
-        List<WifiConfiguration> configuredNetworkList = wifiManager.getConfiguredNetworks();
-        for (int i = 0; i < configuredNetworkList.size(); i++) {
-            String currentSSID = configuredNetworkList.get(i).SSID.replace("\"", "");
-            list.add(currentSSID);
-        }
-        return list;
-    }
-
-    /**
-     * Called when the user clicks the Send button
-     */
-//    public void sendMessage(View view) {
-//        Intent intent = new Intent(this, DisplayMessageActivity.class);
-//        EditText editText = (EditText) findViewById(R.id.edit_message);
-//        String message = editText.getText().toString();
-//
-//        intent.putExtra(EXTRA_MESSAGE, message);
-//        startActivity(intent);
-//    }
     @Override
     public void onStart() {
         super.onStart();
@@ -85,7 +79,7 @@ public class Dashboard extends AppCompatActivity {
         client.connect();
         Action viewAction = Action.newAction(
                 Action.TYPE_VIEW, // TODO: choose an action type.
-                "Dashboard Page", // TODO: Define a title for the content shown.
+                "Login Page", // TODO: Define a title for the content shown.
                 // TODO: If you have web page content that matches this app activity's content,
                 // make sure this auto-generated web page URL is correct.
                 // Otherwise, set the URL to null.
@@ -104,7 +98,7 @@ public class Dashboard extends AppCompatActivity {
         // See https://g.co/AppIndexing/AndroidStudio for more information.
         Action viewAction = Action.newAction(
                 Action.TYPE_VIEW, // TODO: choose an action type.
-                "Dashboard Page", // TODO: Define a title for the content shown.
+                "Login Page", // TODO: Define a title for the content shown.
                 // TODO: If you have web page content that matches this app activity's content,
                 // make sure this auto-generated web page URL is correct.
                 // Otherwise, set the URL to null.
